15f0954a1de333032c1e254fedf0caa07006156f
gbga00 <louboarman@yahoo.com>
2018-03-19 12:29:36 -0400

    Implementing HTML

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hello from Gazoo API app.js file in Amazon, in repo!')
-})
+// app.get('/', (req, res) => {
+  // res.send('Hello from Gazoo API app.js file in Amazon, in repo!')
+// })
+// app.listen(3000, () => console.log('Server running on port 3000'))
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
