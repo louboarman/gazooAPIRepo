f09d1f148877ab69eb2bf9ed4a16538b91f5d553
gbga00 <louboarman@yahoo.com>
2018-03-23 13:29:32 -0400

    removed some cors parameters in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,10 @@ router.get('/api/players', db.getAllPlayers);
 
 // application -------------------------------------------------------------
 router.get('/', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    //res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
     res.render('index', {title: 'gazooapirepo'}); // load the single view file (angular will handle the page changes on the front-end)
 });
