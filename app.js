8cce23e163f90108a3f13102a8f1e95ec329ad7a
gbga00 <louboarman@yahoo.com>
2018-03-23 07:21:39 -0400

    added Cors
    Cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
-// var favicon = require('serve-favicon');
 // var logger = require('morgan');
 // var cookieParser = require('cookie-parser');
 // var bodyParser = require('body-parser');
@@ -8,20 +8,18 @@ var path = require('path');
 var routes = require('./routes/index');
 
 var app = express();
+app.use(cors())
+
+app.use(cors({origin: '*'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
