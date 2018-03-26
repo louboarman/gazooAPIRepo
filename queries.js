a2abc979ce9249ab80a1c94643aa8e0719d3d06d
gbga00 <louboarman@yahoo.com>
2018-03-26 10:38:27 -0400

    dddd

--- queries.js
+++ queries.js
@@ -5,7 +5,7 @@ var options = {
   promiseLib: promise
 };
 
-var pgp = require('pg-promise')(options);
+//var pgp = require('pg-promise')(options);
 //var connectionString = 'postgres://localhost:5432/puppies';
 //var db = pgp(connectionString);
 var cn = {
@@ -16,38 +16,22 @@ var cn = {
     password: '1245KKoo@'
 };
 
-// var db = pgp(cn);
+//var db = pgp(cn);
 
-// var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "yourusername",
-//   password: "yourpassword",
-//   database: "mydb"
-// });
-function getAllPlayers(){
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT name FROM player", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+function getAllPlayers(req, res, next) {
+  db.any('SELECT name FROM player')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          //status: 'success',
+          players: data[],
+          //message: 'Retrieved all players'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
 }
-
-// function getAllPlayers(req, res, next) {
-//   db.any('SELECT name FROM player')
-//     .then(function (data) {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           players: data,
-//           message: 'Retrieved all players'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
 
 
 module.exports = {
